489d5939c36e977302150b872ada0fe0878b3b7f
nijepa <nijepa@gmail.com>
2020-10-03 20:49:48 +0200

    cors refacored

--- src/app.js
+++ src/app.js
@@ -10,7 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+      credentials: true,
+      origin: true
+  })
+);
+app.options('*', cors());
+//app.use(cors());
 
 app.use(async (req, res, next) => {
   req.context = {
